ba66a0a4647f3638950dbf246e8d35ffce469318
sei <teatime.0704@gmail.com>
2020-10-27 22:33:15 +0900

    [add] PlayerController.comment

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -6,18 +6,33 @@ use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
+    /**
+     * 03_問題画面
+     */
     public function question() {
 
         return view('player.question');
     }
+
+    /**
+     * 04_解説画面
+     */
     public function commentary() {
         
         return view('player.commentary');
     }
+
+    /**
+     * 05_マイページ画面
+     */
     public function my_page() {
         
         return view('player.my_page');
     }
+
+    /**
+     * 06_抽選申込画面
+     */
     public function challenge() {
         
         return view('player.challenge');
