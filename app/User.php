85bbcc905865b0e5a88e89fc66dd0e1113bedebf
sei <teatime.0704@gmail.com>
2020-10-27 22:18:17 +0900

    [add] authenticatable

--- app/User.php
+++ app/User.php
@@ -2,12 +2,33 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use Notifiable;
     use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'student_num', 'password', 'is_admin', 'course_id', 'point',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
 
     // 子テーブル
     public function answers()
