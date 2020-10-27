1dbfbdf8f2921e32f6ed326c98b630c1f903b07e
sei <teatime.0704@gmail.com>
2020-10-27 21:08:25 +0900

    [add] model に SoftDeletes 追加

--- app/Gift.php
+++ app/Gift.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Gift extends Model
 {
-    //
+    use SoftDeletes;
 }
--- app/UserGift.php
+++ app/UserGift.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserGift extends Model
 {
-    //
+    use SoftDeletes;
 }
