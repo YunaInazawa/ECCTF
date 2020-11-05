eb4612deab9057486fffedf6a459880bc5a4db02
sei <teatime.0704@gmail.com>
2020-11-05 12:15:19 +0900

    [seed] 問題データ・選択肢データ追加

--- database/seeds/ChoicesTableSeeder.php
+++ database/seeds/ChoicesTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Carbon\carbon;
+use App\Question;
 
 class ChoicesTableSeeder extends Seeder
 {
@@ -13,49 +14,115 @@ class ChoicesTableSeeder extends Seeder
     public function run()
     {
         $now = Carbon::now();
-        $texts = 
-        [
-            'TEST - ANSWER1', 'TEST - ANSWER2', 'TEST - ANSWER3', 
-            'Q1 - ANSWER1',
-            'Q2 - ANSWER1',
-            'Q3 - ANSWER1', 'Q3 - ANSWER2',
-            'Q4 - ANSWER1', 'Q4 - ANSWER2', 'Q4 - ANSWER3', 'Q4 - ANSWER4', 'Q4 - ANSWER5', 'Q4 - ANSWER6',
-            'Q5 - ANSWER1', 
-            'Q6 - ANSWER1', 
-            '...'
-        ];
-        $is_corrects = 
-        [
-            true, true, true,
-            true,
-            true,
-            true, false,
-            false, true, false, false, false, true,
-            true, 
-            true, 
-            true
-        ];
-        $questions = 
-        [
-            'TEST', 'TEST', 'TEST', 
-            'Q1',
-            'Q2',
-            'Q3', 'Q3',
-            'Q4', 'Q4', 'Q4', 'Q4', 'Q4', 'Q4',
-            'Q5', 
-            'Q6', 
-            '...'
-        ];
+        // $texts = 
+        // [
+        //     'TEST - ANSWER1', 'TEST - ANSWER2', 'TEST - ANSWER3', 
+        //     'Q1 - ANSWER1',
+        //     'Q2 - ANSWER1',
+        //     'Q3 - ANSWER1', 'Q3 - ANSWER2',
+        //     'Q4 - ANSWER1', 'Q4 - ANSWER2', 'Q4 - ANSWER3', 'Q4 - ANSWER4', 'Q4 - ANSWER5', 'Q4 - ANSWER6',
+        //     'Q5 - ANSWER1', 
+        //     'Q6 - ANSWER1', 
+        //     '...'
+        // ];
+        // $is_corrects = 
+        // [
+        //     true, true, true,
+        //     true,
+        //     true,
+        //     true, false,
+        //     false, true, false, false, false, true,
+        //     true, 
+        //     true, 
+        //     true
+        // ];
+        // $questions = 
+        // [
+        //     'TEST', 'TEST', 'TEST', 
+        //     'Q1',
+        //     'Q2',
+        //     'Q3', 'Q3',
+        //     'Q4', 'Q4', 'Q4', 'Q4', 'Q4', 'Q4',
+        //     'Q5', 
+        //     'Q6', 
+        //     '...'
+        // ];
 
-        for( $i = 0; $i < count($texts); $i++ ){
-            $question_id = DB::table('questions')->where('text', $questions[$i])->first()->id;
+        // for( $i = 0; $i < count($texts); $i++ ){
+        //     $question_id = DB::table('questions')->where('text', $questions[$i])->first()->id;
+        //     DB::table('choices')->insert([
+        //         'text' => $texts[$i],
+        //         'is_correct' => $is_corrects[$i],
+        //         'question_id' => $question_id,
+        //         'created_at' => $now, 
+        //         'updated_at' => $now,
+        //     ]);
+        // }
+
+        // テストデータ
+        $question_id = Question::where('text', 'Q.TEST : TEXT')->first()->id;
+        for( $i = 1; $i <= 3; $i++ ){
             DB::table('choices')->insert([
-                'text' => $texts[$i],
-                'is_correct' => $is_corrects[$i],
+                'text' => 'Q.TEST : ANSWER' . $i,
+                'is_correct' => true,
                 'question_id' => $question_id,
                 'created_at' => $now, 
                 'updated_at' => $now,
             ]);
+        }
+
+        for( $i = 1; $i <= 54; $i++ ){
+            $question = Question::where('text', 'Q.' . $i . ' : TEXT')->first();
+
+            if( $question->type->name == '択一クイズ' ) {
+                $ans = array(true, false, false, false);
+                shuffle($ans);
+
+                for( $j = 1; $j <= 4; $j++ ){
+                    DB::table('choices')->insert([
+                        'text' => 'Q.' . $i . ' : ANSWER' . $j,
+                        'is_correct' => $ans[$j-1],
+                        'question_id' => $question->id,
+                        'created_at' => $now, 
+                        'updated_at' => $now,
+                    ]);
+                }
+
+            } elseif( $question->type->name == '二択クイズ' ) {
+                $ans = array(true, false);
+                shuffle($ans);
+
+                for( $j = 1; $j <= 2; $j++ ){
+                    DB::table('choices')->insert([
+                        'text' => 'Q.' . $i . ' : ANSWER' . $j,
+                        'is_correct' => $ans[$j-1],
+                        'question_id' => $question->id,
+                        'created_at' => $now, 
+                        'updated_at' => $now,
+                    ]);
+                }
+
+            } elseif( $question->type->name == '多答クイズ' ) {
+                for( $j = 1; $j <= 6; $j++ ){
+                    DB::table('choices')->insert([
+                        'text' => 'Q.' . $i . ' : ANSWER' . $j,
+                        'is_correct' => mt_rand(0, 1),
+                        'question_id' => $question->id,
+                        'created_at' => $now, 
+                        'updated_at' => $now,
+                    ]);
+                }
+
+            } else {
+                DB::table('choices')->insert([
+                    'text' => 'Q.' . $i . ' : ANSWER',
+                    'is_correct' => true,
+                    'question_id' => $question->id,
+                    'created_at' => $now, 
+                    'updated_at' => $now,
+                ]);
+            }
+            
         }
     }
 }
--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -13,23 +13,60 @@ class QuestionsTableSeeder extends Seeder
     public function run()
     {
         $now = Carbon::now();
-        $texts = ['TEST', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', '...'];
-        $types = ['択一クイズ', '穴抜けコード', '一問一答', '二択クイズ', '多答クイズ', '穴抜けコード', '一問一答', '択一クイズ'];
-        $levels = ['TEST', '上級', '上級', '共通', '共通', '下級', '下級', '共通'];
-        $genres = ['TEST', '勉学/プログラミング', '勉学/国家試験', '学校/全体', '学校/先生', '勉学/プログラミング', '勉学/国家試験', '学校/生徒'];
-        
-        for( $i = 0; $i < count($texts); $i++ ){
-            $type_id = DB::table('types')->where('name', $types[$i])->first()->id;
-            $level_id = DB::table('levels')->where('name', $levels[$i])->first()->id;
-            $genre_id = DB::table('genres')->where('name', $genres[$i])->first()->id;
-            DB::table('questions')->insert([
-                'text' => $texts[$i],
-                'type_id' => $type_id,
-                'level_id' => $level_id,
-                'genre_id' => $genre_id,
-                'created_at' => $now, 
-                'updated_at' => $now,
-            ]);
+
+        // $texts = ['TEST', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', '...'];
+        // $types = ['択一クイズ', '穴抜けコード', '一問一答', '二択クイズ', '多答クイズ', '穴抜けコード', '一問一答', '択一クイズ'];
+        // $levels = ['TEST', '上級', '上級', '共通', '共通', '下級', '下級', '共通'];
+        // $genres = ['TEST', '勉学/プログラミング', '勉学/国家試験', '学校/全体', '学校/先生', '勉学/プログラミング', '勉学/国家試験', '学校/生徒'];
+
+        // for( $i = 0; $i < count($texts); $i++ ){
+        //     $type_id = DB::table('types')->where('name', $types[$i])->first()->id;
+        //     $level_id = DB::table('levels')->where('name', $levels[$i])->first()->id;
+        //     $genre_id = DB::table('genres')->where('name', $genres[$i])->first()->id;
+        //     DB::table('questions')->insert([
+        //         'text' => $texts[$i],
+        //         'type_id' => $type_id,
+        //         'level_id' => $level_id,
+        //         'genre_id' => $genre_id,
+        //         'created_at' => $now, 
+        //         'updated_at' => $now,
+        //     ]);
+        // }
+
+        // テストデータ
+        $type_id = DB::table('types')->where('name', '択一クイズ')->first()->id;
+        $level_id = DB::table('levels')->where('name', 'TEST')->first()->id;
+        $genre_id = DB::table('genres')->where('name', 'TEST')->first()->id;
+        DB::table('questions')->insert([
+            'text' => 'Q.TEST : TEXT',
+            'type_id' => $type_id,
+            'level_id' => $level_id,
+            'genre_id' => $genre_id,
+            'created_at' => $now, 
+            'updated_at' => $now,
+        ]);
+
+        $genres = ['学校/全体', '学校/先生', '学校/生徒', '勉学/PC基礎', '勉学/国家試験', '勉学/プログラミング'];
+        $levels = ['共通', '上級', '下級'];
+        $types = ['択一クイズ', '二択クイズ', '多答クイズ', '穴抜けコード', '一問一答'];
+
+        $count = 1;
+        for( $z = 0; $z < 3; $z++ ){
+            for( $i = 0; $i < count($genres); $i++ ){
+                for( $j = 0; $j < count($levels); $j++ ){
+                    $type_id = DB::table('types')->where('name', $types[mt_rand(0, 4)])->first()->id;
+                    $level_id = DB::table('levels')->where('name', $levels[$j])->first()->id;
+                    $genre_id = DB::table('genres')->where('name', $genres[$i])->first()->id;
+                    DB::table('questions')->insert([
+                        'text' => 'Q.' . ($count++) . ' : TEXT',
+                        'type_id' => $type_id,
+                        'level_id' => $level_id,
+                        'genre_id' => $genre_id,
+                        'created_at' => $now, 
+                        'updated_at' => $now,
+                    ]);
+                }
+            }
         }
     }
 }
