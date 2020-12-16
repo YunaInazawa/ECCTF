dc050d8cc8569be90f61a9d6f5491c0952c088fd
sei <teatime.0704@gmail.com>
2020-12-17 05:17:49 +0900

    [add] 一応エラー出ない

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -47,18 +47,18 @@ class QuestionsTableSeeder extends Seeder
         foreach( $file as $line ){
             $text = mb_convert_encoding($line[0], 'UTF-8', 'SJIS');
             $typeName = mb_convert_encoding($line[1], 'UTF-8', 'SJIS');
-            $levelName = mb_convert_encoding($line[2], 'UTF-8', 'SJIS');
-            $genreName = mb_convert_encoding($line[3], 'UTF-8', 'SJIS');
+            $genreName = mb_convert_encoding($line[2], 'UTF-8', 'SJIS');
+            $levelName = mb_convert_encoding($line[3], 'UTF-8', 'SJIS');
 
             $type_id = DB::table('types')->where('name', $typeName)->first()->id;
-            $level_id = DB::table('levels')->where('name', $levelName)->first()->id;
             $genre_id = DB::table('genres')->where('name', $genreName)->first()->id;
+            $level_id = DB::table('levels')->where('name', $levelName)->first()->id;
         
             $list[] = [
                 'text' => $text,
                 'type_id' => $type_id,
-                'level_id' => $level_id,
                 'genre_id' => $genre_id,
+                'level_id' => $level_id,
                 'created_at' => $now, 
                 'updated_at' => $now,
             ];
@@ -67,27 +67,27 @@ class QuestionsTableSeeder extends Seeder
         DB::table('questions')->insert($list);
 
         // テストデータ
-        $genres = ['学校/全体', '学校/先生', '学校/生徒', '勉学/PC基礎', '勉学/国家試験', '勉学/プログラミング'];
-        $levels = ['共通', '上級', '下級'];
-        $types = ['択一クイズ', '二択クイズ', '多答クイズ', '穴抜けコード', '一問一答'];
+        // $genres = ['学校/全体', '学校/先生', '学校/生徒', '勉学/授業', '勉学/PC基礎', '勉学/国家試験', '勉学/プログラミング'];
+        // $levels = ['共通', 'IT共通', 'IT下級', 'IT上級', 'GM共通', 'GM下級', 'GM上級'];
+        // $types = ['択一クイズ', '二択クイズ', '多答クイズ', '穴抜けコード', '一問一答'];
 
-        $count = 1;
-        for( $z = 0; $z < 3; $z++ ){
-            for( $i = 0; $i < count($genres); $i++ ){
-                for( $j = 0; $j < count($levels); $j++ ){
-                    $type_id = DB::table('types')->where('name', $types[mt_rand(0, 4)])->first()->id;
-                    $level_id = DB::table('levels')->where('name', $levels[$j])->first()->id;
-                    $genre_id = DB::table('genres')->where('name', $genres[$i])->first()->id;
-                    DB::table('questions')->insert([
-                        'text' => 'Q.' . ($count++) . ' : TEXT',
-                        'type_id' => $type_id,
-                        'level_id' => $level_id,
-                        'genre_id' => $genre_id,
-                        'created_at' => $now, 
-                        'updated_at' => $now,
-                    ]);
-                }
-            }
-        }
+        // $count = 1;
+        // for( $z = 0; $z < 3; $z++ ){
+        //     for( $i = 0; $i < count($genres); $i++ ){
+        //         for( $j = 0; $j < count($levels); $j++ ){
+        //             $type_id = DB::table('types')->where('name', $types[mt_rand(0, 4)])->first()->id;
+        //             $level_id = DB::table('levels')->where('name', $levels[$j])->first()->id;
+        //             $genre_id = DB::table('genres')->where('name', $genres[$i])->first()->id;
+        //             DB::table('questions')->insert([
+        //                 'text' => 'Q.' . ($count++) . ' : TEXT',
+        //                 'type_id' => $type_id,
+        //                 'level_id' => $level_id,
+        //                 'genre_id' => $genre_id,
+        //                 'created_at' => $now, 
+        //                 'updated_at' => $now,
+        //             ]);
+        //         }
+        //     }
+        // }
     }
 }
